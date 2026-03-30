66ca89ee84a7892cebc336808dbcbf71874dc407
Alonso <donayrealonso987@gmail.com>
2026-03-29 21:16:10 -0600

    Actualizando proyecto

---
+++ js/bienvenida.js
@@ -0,0 +1,9 @@
+// Solicitar al usuario que ingrese su nombre
+let nombre = prompt("Por favor, ingresa tu nombre:");
+let edad = prompt("Por favor, ingresa tu edad:");
+
+// Mostrar el resultado en la página
+alert("El usuario " + nombre + " Tiene " + edad + " años.");
+
+// Mostrar el resultado en la página
+document.write("El usuario " + nombre + " Tiene " + edad + " años.");
