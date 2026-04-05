25394993231cd670529429d6b6ecb3cb6fd29f34
Alonso <donayrealonso987@gmail.com>
2026-04-05 16:49:05 -0600

    Ultima actualizacion del logout, terminada

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,7 @@
 <?php
+// iniciamos la sesion con session start y verificamos si el usuario esta logueado, si no lo esta lo redirigimos al login
 session_start();
-
+// el isset verifica si la variable contiene algun valor 
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: login2.html");
     exit();
@@ -11,9 +12,18 @@ if (!isset($_SESSION['usuario_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LOGIN</title>
+    <!-- Libreria de Icons -->
+    <script src="https://kit.fontawesome.com/812c8ee19a.js" crossorigin="anonymous"></script>
+    <!-- Framework de Bootsrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Estilos de css -->
+    <link rel="stylesheet" href="css/styles_index.css">
+    <!-- Favicon de la aplicacion de matricula -->
+    <link rel="shortcut icon" href="img/faviconn (1).ico" type="image/x-icon">
 </head>
 <body>
-    <a href="php/logout.php">Cerrar sesión</a>
+    <a href="php/logout.php" class="btn btn-danger">Cerrar sesión</a>
 </body>
 </html>
--- php/logout.php
+++ php/logout.php
@@ -1,4 +1,5 @@
 <?php
+// logout.php: Este archivo se encarga de cerrar la sesión del usuario y redirigirlo al login
 session_start();
 session_unset();
 session_destroy();
